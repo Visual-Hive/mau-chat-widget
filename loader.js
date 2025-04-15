d70e1b202c576cdc56bf6dc7e79417a311d2930e
Richard Osborne <richard@digitalbricks.io>
2025-04-15 10:17:30 +0200

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,6 @@
+
+// Create a script element to load the chat widget
+const script = document.createElement('script');
+script.src = 'https://visual-hive.github.io/mau-chat-widget/mau-chat-widget.js';
+script.async = true;
+document.head.appendChild(script);
