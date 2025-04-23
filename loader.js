83c575a149fec9a86d9e1d5e3145a2e29145ffb2
Richard Osborne <richard@digitalbricks.io>
2025-04-23 15:06:42 +0200

    Added variable to ensure chatbot opens on page load

--- loader.js
+++ loader.js
@@ -1,4 +1,8 @@
+// Create a script element to load the chat widget with defer attribute
 const script = document.createElement('script');
 script.src = 'https://visual-hive.github.io/mau-chat-widget/mau-chat-widget.js';
 script.defer = true; // Use defer instead of async
 document.head.appendChild(script);
+
+// Add a flag to indicate we want to open the chat automatically after load
+window.mauChatAutoOpen = true;
