b4c66f0aadcd42c70f4a33a1dfe69bcdaa472c48
Richard Osborne <richard@digitalbricks.io>
2025-04-23 14:55:49 +0200

    Changed to deferred loading

--- loader.js
+++ loader.js
@@ -1,6 +1,4 @@
-
-// Create a script element to load the chat widget
 const script = document.createElement('script');
 script.src = 'https://visual-hive.github.io/mau-chat-widget/mau-chat-widget.js';
-script.async = true;
+script.defer = true; // Use defer instead of async
 document.head.appendChild(script);
